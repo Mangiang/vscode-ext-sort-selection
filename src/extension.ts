f3218a620256060dc5a0ad0b263f21d9cc741e2a
Mangiang <mangiang@orange.fr>
2020-04-10 19:40:18 -0400

    feat(sort): Add the sorting

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,107 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, commands, window, TextEditor, Range, Selection } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sort-selection" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let disposable = commands.registerCommand('extension.sort-array', () => {
+		const editor = window.activeTextEditor;
+		if (!editor) { return; }
+		const selectionStr: string = editor.document.getText(new Range(editor.selection.start, editor.selection.end));
+		const arr = getArray(selectionStr);
+		replaceArray(editor, editor.selection, `[${arr.join(',')}]`);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function getArray(selectionStr: string): Array<string> {
+	const match: RegExpMatchArray | null = selectionStr.match(/\[((.*\n*\s*)+)\]/m);
+	if (!match) { return []; };
+
+	const matchStr = match[1];
+
+	const rootCommaIdxList: Array<number> = [];
+	let backQuoteCeption = 0;
+	let singleQuoteCeption = 0;
+	let doubleQuoteCeption = 0;
+	let curlyBracesCeption = 0;
+	let bracketsCeption = 0;
+	let parenthesesCeption = 0;
+
+	for (let idx = 0; idx < matchStr.length; idx += 1) {
+		const currChar = matchStr[idx];
+		switch (currChar) {
+			case ',':
+				if (!backQuoteCeption && !singleQuoteCeption && !doubleQuoteCeption && !curlyBracesCeption && !bracketsCeption && !parenthesesCeption) { rootCommaIdxList.push(idx); }
+				break;
+			case '\`':
+				if (backQuoteCeption % 2 === 0) { backQuoteCeption += 1; }
+				else {
+					backQuoteCeption -= 1;
+				}
+				break;
+			case '\'':
+				if (singleQuoteCeption % 2 === 0) { singleQuoteCeption += 1; }
+				else {
+					singleQuoteCeption -= 1;
+				}
+				break;
+			case '\"':
+				if (doubleQuoteCeption % 2 === 0) { doubleQuoteCeption += 1; }
+				else {
+					doubleQuoteCeption -= 1;
+				}
+				break;
+			case '{':
+				curlyBracesCeption += 1;
+				break;
+			case '}':
+				curlyBracesCeption -= 1;
+				break;
+			case '[':
+				bracketsCeption += 1;
+				break;
+			case ']':
+				bracketsCeption -= 1;
+				break;
+			case '(':
+				parenthesesCeption += 1;
+				break;
+			case ')':
+				parenthesesCeption -= 1;
+				break;
+			default:
+				break;
+		}
+	}
+	if (rootCommaIdxList.length === 0) { return []; };
+
+	const strArr: Array<string> = [];
+	strArr.push(matchStr.substr(0, rootCommaIdxList[0]));
+	for (let idx = 0; idx < rootCommaIdxList.length - 1; idx += 1) {
+		strArr.push(matchStr.substr(rootCommaIdxList[idx] + 1, rootCommaIdxList[idx + 1] - rootCommaIdxList[idx] - 1));
+	}
+	strArr.push(matchStr.substr(rootCommaIdxList[rootCommaIdxList.length - 1] + 1, matchStr.length - rootCommaIdxList[rootCommaIdxList.length - 1] - 1));
+
+	const spaceArr: Array<string> = [];
+	for (let idx = 0; idx < strArr.length; idx += 1) {
+		const matchSpace: RegExpMatchArray | null = strArr[idx].match(/\n+\s*/);
+		if (matchSpace) {
+			spaceArr.push(matchSpace[0]);
+		} else {
+			spaceArr.push('');
+		}
+	}
+
+	const trimmedArr = strArr.map(it => it.trim());
+	const sortedTrimmedStrArr = trimmedArr.sort();
+	const sortedStrArr = sortedTrimmedStrArr.map((elt, idx) => spaceArr[idx] + elt);
+	return sortedStrArr;
+}
+
+function replaceArray(editor: TextEditor, selection: Selection, newString: string) {
+	editor.edit(editBuilder => {
+		editBuilder.replace(selection, newString);
+	});
+}
+
+export function deactivate() { }
